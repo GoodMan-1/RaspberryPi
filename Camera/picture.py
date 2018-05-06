d360da4b60031a45ef89c442f5e3022d51221128
杨崟 <38201648+yangyin1019@users.noreply.github.com>
2018-05-06 23:12:26 +0800

    Create picture.py

---
+++ Camera/picture.py
@@ -0,0 +1,8 @@
+from picamera import PiCamera
+import time
+camera = PiCamera()
+camera.start_preview()
+time.sleep(1)
+date=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+camera.capture('/home/pi/jpg/%s.jpg'% date)   #照片路径/home/pi/jpg,名称为拍照的时间
+camera.stop_preview()
