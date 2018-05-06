78ba3ac3dc49bf2fbcd7b89bd9fced725c9d0a45
杨崟 <38201648+yangyin1019@users.noreply.github.com>
2018-05-06 23:15:38 +0800

    拍照python脚本

--- Camera/picture.py
+++ Camera/picture.py
@@ -4,5 +4,5 @@ camera = PiCamera()
 camera.start_preview()
 time.sleep(1)
 date=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-camera.capture('/home/pi/jpg/%s.jpg'% date)   #照片路径/home/pi/jpg,名称为拍照的时间
+camera.capture('/home/pi/jpg/%s.jpg'% date)   #照片路径/home/pi/jpg,名称为拍照时间
 camera.stop_preview()
